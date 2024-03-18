35b1aefeba5f14b3356ab440b543e6e68ef60dc3
lilililit <rokogamesjp@gmail.com>
2024-03-18 16:00:38 +0100

    uid
    dodane uid

---
+++ Assets/scripts/cows_and_bulls.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cowsandbulls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string secretWord = "hello";
+        int playerLives = 3;
+        Debug.Log("The secret word is [" + secretWord +"]");
+        Debug.Log("You have" + playerLives + "lives left");
+        Debug.Log("You have "+ playerLives + "lives left");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
