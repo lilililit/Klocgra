b5b896da446286fc9f45ffb22f33b6fb0030b570
lilililit <rokogamesjp@gmail.com>
2024-03-18 16:20:36 +0100

    zastopinie debug.log tekstem

--- Assets/scripts/cows_and_bulls.cs
+++ Assets/scripts/cows_and_bulls.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class cowsandbulls : MonoBehaviour
 {
+    public TextMeshProUGUI textSecretWord;
+    public TextMeshProUGUI textSecretWordLength;
+    public TextMeshProUGUI textLivesCount;
+    public string secretWord = "hello";
+    public int playerLives = 3;
+
     // Start is called before the first frame update
     void Start()
     {
-        string secretWord = "hello";
-        int playerLives = 3;
-        Debug.Log("The secret word is [" + secretWord +"]");
-        Debug.Log("You have" + playerLives + "lives left");
-        Debug.Log("You have "+ playerLives + "lives left");
+        
+        textSecretWord.text ="The secret word is [" + secretWord +"]";
+        textSecretWordLength.text="The length of the secret word is " + secretWord.Length;
+        textLivesCount.text = "You have "+ playerLives + "lives left";
 
 
     }
