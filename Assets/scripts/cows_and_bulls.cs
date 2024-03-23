bc8d66ef402ac776abb64f7c0f47d863376c2653
lilililit <rokogamesjp@gmail.com>
2024-03-23 14:36:13 +0100

    dodane funkcje zabierania zyc,skonczony skrypt

--- Assets/scripts/cows_and_bulls.cs
+++ Assets/scripts/cows_and_bulls.cs
@@ -11,7 +11,42 @@ public class cowsandbulls : MonoBehaviour
     public string secretWord = "hello";
     public int playerLives = 3;
 
-   
+    public void RemoveLife()
+    {
+        playerLives--;
+        textLivesCount.text = "You have " + playerLives + "Lives left";
+        if (playerLives <= 0) 
+        {
+            textSecretWord.text = "the word was :" + secretWord;
+            textSecretWordLength.text = " Game  over";
+            textLivesCount.text = "You have no lives left";
+        }
+    }
+    public int CountBulls(string origin, string guess)
+    {
+        int bullsCount = 0;
+        for (int i = 0;i < origin.Length; i++) 
+        {
+            if (origin[i] == guess[i])
+            {
+                bullsCount++;
+            }
+        }
+        return bullsCount;
+    }
+    public int CountCows(string origin, string guess)
+    {
+        int cowsCount = 0;
+        for (int i = 0; i < origin.Length; i++)
+        {
+            if (origin.Contains(guess[i] + "") && origin[i] != guess[i])
+            {
+                cowsCount++;
+            }
+        }
+        return cowsCount;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +65,38 @@ public class cowsandbulls : MonoBehaviour
     }
     public void OnButtonClick(TMP_InputField input)
     {
-        Debug.Log("Input was : " + input.text);
+        if(playerLives <= 0) 
+        {
+            return;
+        }
+        if (input.text == secretWord) 
+        {
+         Win();
+         return;
+
+        }
+        
+        if (secretWord.Length != input.text.Length) 
+        {
+            RemoveLife();
+            Debug.Log("Wrong length, try again");
+            textSecretWord.text = "wrong length ! Try again and press Submit!";
+        }
+        else 
+        {
+            RemoveLife();
+        int bulls =CountBulls(secretWord, input.text);
+        int cows = CountCows(secretWord, input.text);
+
+            textSecretWord.text = "bulls :" + bulls +" " + "Cows :" + cows + " " + "Try again and press enter";
+        }
+        input.text = "";
+    }
+
+    public void Win() 
+    {
+        textSecretWord.text = "The word was :" + secretWord;
+        textSecretWordLength.text = "Congratulations!!";
+        textLivesCount.text = "You have " + playerLives + "Lives left";
     }
 }
